284d198cab90fe3698b0b9a1b02f005b4c12ca1d
Steven Kornajcik Jr <stevenkornajcik@wireless-10-147-252-99.public.utexas.edu>
2018-05-14 18:49:50 -0500

    Add existing file

--- HW_13_D3/Skeleton/assets/js/app.js
+++ HW_13_D3/Skeleton/assets/js/app.js
@@ -11,7 +11,7 @@ var margin = {top: 20, right: 40, bottom: 60, left: 100};
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+
 var svg = d3
   .select('.chart')
   .append('svg')
@@ -21,10 +21,7 @@ var svg = d3
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 var chart = svg.append('g');
 
-// Append a div to the body to create tooltips, assign it a class
-d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
-
-// Retrieve data from CSV file and execute everything below
+// CSV
 d3.csv("../../data/data.csv", function(err, healthData) {
     if(err) throw err;
 
@@ -33,15 +30,7 @@ d3.csv("../../data/data.csv", function(err, healthData) {
         data.phys_act = +data.phys_act;
     });
     
-    // Create scale functions
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
-    var xLinearScale = d3.scaleLinear().range([0, width]);
-
-    // Create axis functions
-    var bottom_Axis = d3.axisBottom(xLinearScale);
-    var left_Axis = d3.axisLeft(yLinearScale);
-
-    // Scale the domain
+    // domain
     var xMin;
     var xMax;
     var yMin;
@@ -66,22 +55,24 @@ d3.csv("../../data/data.csv", function(err, healthData) {
     xLinearScale.domain([xMin, xMax]);
     yLinearScale.domain([yMin, yMax]);
 
-    
+
+d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+
     // Initialize tooltip 
     var toolTip = d3
         .tip()
         .attr("class", "tooltip")
         .offset([80, -60])
         .html(function(data) {
-            var stateName = data.state;
+            var stateN = data.state;
             var pov = +data.poverty;
             var physAct = +data.phys_act;
             return (
-                stateName + '<br> Poverty: ' + pov + '% <br> Physically Active: ' + physAct +'%'
+                stateN + '<br> Poverty: ' + pov + '% <br> Physically Active: ' + physAct +'%'
             );
         });
 
-    // Create tooltip
+    // make tooltip
     chart.call(toolTip);
 
     chart.selectAll("circle")
@@ -98,7 +89,7 @@ d3.csv("../../data/data.csv", function(err, healthData) {
         .attr("fill", "lightblue")
         
     
-    // Appending a label to each data point
+    // label each data point
     chart.append("text")
         .style("text-anchor", "middle")
         .style("font-size", "12px")
@@ -116,13 +107,19 @@ d3.csv("../../data/data.csv", function(err, healthData) {
                 return data.abbr
             });
     
-    // Append an SVG group for the xaxis, then display x-axis 
+    // x-axis 
     chart
         .append("g")
         .attr('transform', `translate(0, ${height})`)
         .call(bottomAxis);
-
-    // Append a group for y-axis, then display it
+  // x-axis labels
+  chart
+  .append("text")
+  .attr(
+      "transform",
+      "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
+  )
+    // y-axis
     chart.append("g").call(leftAxis);
 
     // Append y-axis label
@@ -131,14 +128,6 @@ d3.csv("../../data/data.csv", function(err, healthData) {
         .attr("transform", "rotate(-90)")
         .attr("y", 0-margin.left + 40)
         .attr("x", 0 - height/2)
-  
 
-    // Append x-axis labels
-    chart
-        .append("text")
-        .attr(
-            "transform",
-            "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
-        )
-    
+
     });
